328355610cbba4abf7ecda23dddcbe2201c9f189
Pablo Espinosa Arias <69204800+pea0715@users.noreply.github.com>
2022-02-23 00:24:21 -0500

    Rename point4.sql.sql to point4.sql

--- Atelier 1/point4.sql
+++ Atelier 1/point4.sql
@@ -4,4 +4,4 @@ FROM productos p
 INNER JOIN detalles d ON p.id_producto = d.id_producto
 INNER JOIN facturas f ON d.id_factura = f.num_factura
 INNER JOIN modo_pagos m ON f.num_pago = m.num_pago
-where (m.nombre != 'tarjeta débito') And (p.nombre like 'C%')
+where (m.nombre != 'tarjeta dÃ©bito') And (p.nombre like 'C%')
