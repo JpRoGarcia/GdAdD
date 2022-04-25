c35d8514da9993cc8971a0952dfeca7446164ce7
Pablo Espinosa Arias <69204800+pea0715@users.noreply.github.com>
2022-04-24 21:39:59 -0500

    agregar consulta sql paciente y empleado

---
+++ Atelier 3/Eps/DaseDatos/EmpleadoDao.cs
@@ -0,0 +1,68 @@
+﻿using Eps.Usuario;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Eps.DaseDatos
+{
+    public class EmpleadoDao
+    {
+        dataBase conectar = new dataBase();
+        public int RegistrarEmpleado(Empleado empleado)
+        {
+            SqlConnection sqlConnection = new SqlConnection(conectar.conexion);
+
+            String query = "INSERT INTO tbEmpleado(Cedula, Nombre, Apellido, Contrasena) VALUES(" +
+                empleado.ECedula + ", '" + empleado.ENombre + "', '" +
+                empleado.EApellido + "', '" + empleado.EContraseña + "')";
+
+            sqlConnection.Open();
+            SqlCommand command = new SqlCommand(query, sqlConnection);
+
+            int rEmpleado;
+
+            rEmpleado = command.ExecuteNonQuery();
+
+            sqlConnection.Close();
+
+            return rEmpleado;
+        }
+
+        public DataSet Autentificar(int Cedula, String Contraseña)
+        {
+            SqlConnection sqlConnection = new SqlConnection(conectar.conexion);
+            String query = "SELECT Cedula, Contrasena  FROM tbEmpleado " +
+                "WHERE Cedula = '" + Cedula + "' and " +
+                "Contrasena = '" + Contraseña + "'";
+
+            sqlConnection.Open();
+            SqlDataAdapter adapter = new SqlDataAdapter(query, sqlConnection);
+
+            DataSet dataSet = new DataSet();
+            adapter.Fill(dataSet, "Iniciar");
+            sqlConnection.Close();
+
+            return dataSet;
+        }
+
+        public DataSet BuscarEmpleado(int Cedula)
+        {
+            SqlConnection sqlConnection = new SqlConnection(conectar.conexion);
+            String query = "SELECT Cedula FROM tbEmpleado " +
+                "WHERE Cedula = '" + Cedula +  "'";
+
+            sqlConnection.Open();
+            SqlDataAdapter adapter = new SqlDataAdapter(query, sqlConnection);
+
+            DataSet dataSet = new DataSet();
+            adapter.Fill(dataSet, "BuscarE");
+            sqlConnection.Close();
+
+            return dataSet;
+        }
+    }
+}
---
+++ Atelier 3/Eps/DaseDatos/PacienteDao.cs
@@ -0,0 +1,121 @@
+﻿using Eps.Usuario;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Eps.DaseDatos
+{
+    class PacienteDao
+    {
+        dataBase conectar = new dataBase();
+
+        public int RegistrarPaciente(Paciente paciente)
+        {
+            SqlConnection sqlConnection = new SqlConnection(conectar.conexion);
+
+            String query = "INSERT INTO tbPaciente(Cedula, Nombre, Apellido, FechaNacimiento, Direcion, Email, Telefono, FechaRegistro, PazySalvo) VALUES(" +
+                paciente.PNumeroID + ", '" + paciente.PNombre + "', '" +
+                paciente.PApellido + "', '" + paciente.PFechaNacimiento + "', '" +
+                paciente.PDireccion + "', '" + paciente.PEmail + "', '" +
+                paciente.PTelefono + "', '" + paciente.PRegistro + "', '" + paciente.PPazySalvo + "')";
+
+            sqlConnection.Open();
+            SqlCommand command = new SqlCommand(query, sqlConnection);
+            int rPaciente;
+
+            rPaciente = command.ExecuteNonQuery();
+
+            sqlConnection.Close();
+
+            return rPaciente;
+        }
+
+        public int actualizarPaciente(int Cedula, string Email, string Direccion, string Telefono)
+        {
+            SqlConnection sqlConnection = new SqlConnection(conectar.conexion);
+
+            String query = "UPDATE tbPaciente SET Email = '" + Email + "', Direcion = '" + Direccion + "', Telefono = '" + Telefono + "' WHERE Cedula = " + Cedula;
+
+            sqlConnection.Open();
+            SqlCommand command = new SqlCommand(query, sqlConnection);
+            int registros;
+
+            registros = command.ExecuteNonQuery();
+
+            sqlConnection.Close();
+
+            return registros;
+        }
+
+        public int actualizarPazySalvo(int Cedula, String PazySalvo)
+        {
+            SqlConnection sqlConnection = new SqlConnection(conectar.conexion);
+
+            String query = "UPDATE tbPaciente SET PazySalvo = '" + PazySalvo + "' WHERE Cedula = " + Cedula;
+
+            sqlConnection.Open();
+            SqlCommand command = new SqlCommand(query, sqlConnection);
+            int registros;
+
+            registros = command.ExecuteNonQuery();
+
+            sqlConnection.Close();
+
+            return registros;
+        }
+
+        public DataSet BuscarPaciente(int Cedula)
+        {
+            SqlConnection sqlConnection = new SqlConnection(conectar.conexion);
+            String query = "SELECT Cedula FROM tbPaciente " +
+                "WHERE Cedula = '" + Cedula + "'";
+
+            sqlConnection.Open();
+            SqlDataAdapter adapter = new SqlDataAdapter(query, sqlConnection);
+
+            DataSet dataSet = new DataSet();
+            adapter.Fill(dataSet, "BuscarP");
+            sqlConnection.Close();
+
+            return dataSet;
+        }
+
+        public DataSet MostrarPaciente()
+        {
+            SqlConnection sqlConnection = new SqlConnection(conectar.conexion);
+            String query = "SELECT * FROM tbPaciente";
+
+            sqlConnection.Open();
+            SqlDataAdapter adapter = new SqlDataAdapter(query, sqlConnection);
+
+            DataSet dataSet = new DataSet();
+            adapter.Fill(dataSet, "MostrarP");
+            sqlConnection.Close();
+
+            return dataSet;
+        }
+
+        public int EliminarPaciente(int Cedula)
+        {
+            SqlConnection sqlConnection = new SqlConnection(conectar.conexion);
+
+            String query = "DELETE FROM tbPaciente WHERE Cedula = " + Cedula;
+
+            sqlConnection.Open();
+            SqlCommand command = new SqlCommand(query, sqlConnection);
+            int registros;
+
+            registros = command.ExecuteNonQuery();
+
+            sqlConnection.Close();
+
+            return registros;
+        }
+
+
+    }
+}
