cba2a3e2dbbd791723b0ee85054dbf67d0505462
Pablo Espinosa Arias <69204800+pea0715@users.noreply.github.com>
2022-04-24 21:39:10 -0500

    agregar  objetos paciente y multa

---
+++ Atelier 3/Eps/Usuario/Paciente.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Eps.Usuario
+{
+    class Paciente
+    {
+        private int pNumeroID;
+        private String pNombre;
+        private String pApellido;
+        private String pFechaNacimiento;
+        private String pDireccion;
+        private String pEmail;
+        private String pTelefono;
+        private String pRegistro;
+        private String pPazySalvo;
+
+        public Paciente(int pNumeroID, string pNombre, string pApellido, string pFechaNacimiento, string pDireccion, string pEmail, string pTelefono, string pRegistro, string pPazySalvo)
+        {
+            this.pNumeroID = pNumeroID;
+            this.pNombre = pNombre;
+            this.pApellido = pApellido;
+            this.pFechaNacimiento = pFechaNacimiento;
+            this.pDireccion = pDireccion;
+            this.pEmail = pEmail;
+            this.pTelefono = pTelefono;
+            this.pRegistro = pRegistro;
+            this.pPazySalvo = pPazySalvo;
+        }
+
+        public int PNumeroID { get => pNumeroID; set => pNumeroID = value; }
+        public string PNombre { get => pNombre; set => pNombre = value; }
+        public string PApellido { get => pApellido; set => pApellido = value; }
+        public string PFechaNacimiento { get => pFechaNacimiento; set => pFechaNacimiento = value; }
+        public string PDireccion { get => pDireccion; set => pDireccion = value; }
+        public string PTelefono { get => pTelefono; set => pTelefono = value; }
+        public string PEmail { get => pEmail; set => pEmail = value; }
+        public string PRegistro { get => pRegistro; set => pRegistro = value; }
+        public string PPazySalvo { get => pPazySalvo; set => pPazySalvo = value; }
+    }
+}
